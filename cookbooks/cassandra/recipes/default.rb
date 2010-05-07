00eb52085ef9e7fde6393460d62f87693a072c59
Rob Pak <rob@carbonfive.com>
2010-05-07 15:37:30 -0700

    Trying stuff out

--- cookbooks/cassandra/recipes/default.rb
+++ cookbooks/cassandra/recipes/default.rb
@@ -10,16 +10,23 @@
 #  action :install
 #end
 
+include_recipe "java"
+
 node[:applications].each do |app_name, data|
 
   if node[:instance_role] == 'util' && (node[:name] != nil && node[:name].include?("cass"))
+
+#    package "openjdk-6-jre" do
+#      action :install
+#    end
+
     template File.join("/data/#{app_name}/current",'config', 'cassandra', 'storage-conf.xml') do
-    owner node[:owner_name]
-    group node[:owner_name]
-    source 'storage-conf.xml.erb'
-    variables({
-      :app_name => app_name
-    })
+      owner node[:owner_name]
+      group node[:owner_name]
+      source 'storage-conf.xml.erb'
+      variables({
+        :app_name => app_name
+      })
     end
   end
 
---
+++ cookbooks/java/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: java
+# Recipe:: default
+#
+
+package "openjdk-6-jre" do
+  action :install
+end
