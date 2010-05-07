37be75e4370ad8164ee9b2c0c576adfb391e64ae
Rob Pak <rob@carbonfive.com>
2010-05-07 16:50:00 -0700

    Trying stuff out

---
+++ cookbooks/cassandra/libraries/cassandra.rb
@@ -0,0 +1,14 @@
+class Chef
+  class Recipe
+    def configure
+      template File.join("/data/#{app_name}/current",'config', 'cassandra', 'storage-conf.xml') do
+      owner node[:owner_name]
+      group node[:owner_name]
+      source 'storage-conf.xml.erb'
+      variables({
+        :app_name => app_name
+      })
+    end
+    end
+  end
+end
--- cookbooks/cassandra/recipes/default.rb
+++ cookbooks/cassandra/recipes/default.rb
@@ -33,21 +33,8 @@ node[:applications].each do |app_name, data|
 #      }
 #    end
 
-    configure_cassandra
+    configure()
 
   end
 
-end
-
-
-
-def configure_cassandra
-  template File.join("/data/#{app_name}/current",'config', 'cassandra', 'storage-conf.xml') do
-      owner node[:owner_name]
-      group node[:owner_name]
-      source 'storage-conf.xml.erb'
-      variables({
-        :app_name => app_name
-      })
-    end
 end
