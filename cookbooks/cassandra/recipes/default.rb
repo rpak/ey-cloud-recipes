1d52b48b09968244b7917fe81893d02d2cd15ca7
Rob Pak <rob@carbonfive.com>
2010-05-07 16:32:06 -0700

    Trying stuff out

--- cookbooks/cassandra/recipes/default.rb
+++ cookbooks/cassandra/recipes/default.rb
@@ -16,10 +16,13 @@ node[:applications].each do |app_name, data|
 
   if node[:instance_role] == 'util' && (node[:name] != nil && node[:name].include?("cass"))
 
-#    package "openjdk-6-jre" do
-#      action :install
-#    end
     include_recipe "java"
+
+    remote_file "/tmp/cassandra.tar.gz" do
+      source "http://apache.mirror.facebook.net/cassandra/0.6.1/apache-cassandra-0.6.1-bin.tar.gz"
+      mode "0644"
+      checksum "6aa1764f76e26fbc9d62b59e2f6a9ab4"
+    end
 
     template File.join("/data/#{app_name}/current",'config', 'cassandra', 'storage-conf.xml') do
       owner node[:owner_name]
