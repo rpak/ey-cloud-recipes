318a3ad651b4d5a45e1dc83d63c30e71ec646bdc
Rob Pak <rob@carbonfive.com>
2010-05-11 17:44:22 -0700

    Tomcat support

--- cookbooks/tomcat/attributes/tomcat.rb
+++ cookbooks/tomcat/attributes/tomcat.rb
@@ -1,6 +1,6 @@
-set_unless[:tomcat][:version]                 = "6.0.26"
-set_unless[:tomcat][:with_native]             = true
-set_unless[:tomcat][:java_home]               = "/usr/lib/jvm/java"
-set_unless[:tomcat][:java_opts]               = ""
-set_unless[:tomcat][:permgen_min_free_in_mb]  = 24
-set_unless[:tomcat][:checksum]                = "f9eafa9bfd620324d1270ae8f09a8c89"
+default[:tomcat][:version]                 = "6.0.26"
+default[:tomcat][:with_native]             = true
+default[:tomcat][:java_home]               = "/usr/lib/jvm/java"
+default[:tomcat][:java_opts]               = ""
+default[:tomcat][:permgen_min_free_in_mb]  = 24
+default[:tomcat][:checksum]                = "f9eafa9bfd620324d1270ae8f09a8c89"
