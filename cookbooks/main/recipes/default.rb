a54248b6c591483a186601b6a3044c93ed6a828f
Rob Pak <rob@carbonfive.com>
2010-05-13 10:58:20 -0700

    Outputing to log file

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -20,16 +20,16 @@ if node[:name] != nil && node[:name].include?("job")
   cron "br-jobs-aggregate" do
     minute "/5"
     user node[:owner_name]
-    command "cd /data/#{APP_NAME}/current && rake br:jobs:aggregate"
+    command "cd /data/#{APP_NAME}/current && rake br:jobs:aggregate > /data/#{APP_NAME}/shared/log/br-jobs-aggregate.log"
   end
   cron "br-jobs-solr" do
     minute "/2"
     user node[:owner_name]
-    command "cd /data/#{APP_NAME}/current && rake br:jobs:solr"
+    command "cd /data/#{APP_NAME}/current && rake br:jobs:solr > /data/#{APP_NAME}/shared/log/br-jobs-solr.log"
   end
   cron "br-jobs-blast" do
     minute "/2"
     user node[:owner_name]
-    command "cd /data/#{APP_NAME}/current && rake br:jobs:blast"
+    command "cd /data/#{APP_NAME}/current && rake br:jobs:blast > /data/#{APP_NAME}/shared/log/br-jobs-blast.log"
   end
 end
