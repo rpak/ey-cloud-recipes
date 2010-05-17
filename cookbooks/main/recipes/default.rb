eaebe0a33f1f2643f50423cfe806ea89d5f35a76
Rob Pak <rob@carbonfive.com>
2010-05-17 16:48:03 -0700

    Updated cron syntax

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -18,18 +18,22 @@ end
 
 if node[:name] != nil && node[:name].include?("job")
   cron "br-jobs-aggregate" do
-    minute "/5"
+    minute "*/5"
     user node[:owner_name]
     command "cd /data/#{APP_NAME}/current && RAILS_ENV=#{node[:environment][:framework_env]} rake br:jobs:aggregate > /data/#{APP_NAME}/shared/log/br-jobs-aggregate.log"
   end
   cron "br-jobs-solr" do
-    minute "/2"
+    minute "*/2"
     user node[:owner_name]
     command "cd /data/#{APP_NAME}/current && RAILS_ENV=#{node[:environment][:framework_env]} rake br:jobs:solr > /data/#{APP_NAME}/shared/log/br-jobs-solr.log"
   end
   cron "br-jobs-blast" do
-    minute "/2"
+    minute "*/2"
     user node[:owner_name]
     command "cd /data/#{APP_NAME}/current && RAILS_ENV=#{node[:environment][:framework_env]} rake br:jobs:blast > /data/#{APP_NAME}/shared/log/br-jobs-blast.log"
   end
 end
+
+# */5 * * * * cd /data/bitchroom/current && RAILS_ENV=staging rake br:jobs:aggregate > /data/bitchroom/shared/log/br-jobs-aggregate.log
+# */1 * * * * cd /data/bitchroom/current && RAILS_ENV=staging rake br:jobs:solr > /data/bitchroom/shared/log/br-jobs-solr.log
+# */1 * * * * cd /data/bitchroom/current && RAILS_ENV=staging rake br:jobs:blast > /data/bitchroom/shared/log/br-jobs-blast.log
