e71c6eaa5bcfcf0152047a5952b2411830e299a9
Rob Pak <rob@carbonfive.com>
2010-06-10 16:13:30 -0700

    Adding haproxy

--- cookbooks/cassandra_client/recipes/default.rb
+++ cookbooks/cassandra_client/recipes/default.rb
@@ -11,6 +11,13 @@ execute "restart-servers" do
   action :nothing
 end
 
+execute "reload-haproxy" do
+  command %Q{
+    /etc/init.d/haproxy reload
+  }
+  action :nothing
+end
+
 cassandra_nodes = node[:utility_instances].find_all {|v| v[:name].include?("cass")}
 servers = (cassandra_nodes.collect{|n| n[:hostname] + ":9160"}).join ","
 
@@ -24,4 +31,21 @@ template "/data/#{APP_NAME}/current/config/cassandra.yml" do
     :servers => servers
   })
   notifies :run, resources(:execute => "restart-servers")
+end
+
+execute "ha-proxy-add-cassandra" do
+  command %Q{
+    echo '' >> /etc/haproxy.cfg && echo 'listen cassandra :9160' >> /etc/haproxy.cfg
+  }
+  not_if 'grep cassandra /etc/haproxy.cfg' 
+end
+
+cassandra_nodes.each_with_index do |n, i|
+  execute "add-node" do
+    command %Q{
+      echo 'server cass-#{i} #{n[:hostname]}:9160 check inter 5000 fastinter 1000 fall 1 weight 49' >> /etc/haproxy.cfg
+    }
+    not_if "grep #{n[:hostname]}:9160 /etc/haproxy.cfg"
+    notifies :run, resources(:execute => "reload-haproxy")
+  end
 end
